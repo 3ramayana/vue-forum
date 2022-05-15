d6c6ac16a1bcd7bc16d81d6c62385365e15faaef
3ramayana <3ramayana@gmail.com>
2022-05-16 06:44:39 +0900

    add vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import sourceData from '@/data.json';
 
 const PageHome = () => import('@/components/PageHome');
 const PageThreadShow = () => import('@/components/PageThreadShow');
@@ -7,10 +8,26 @@ const PageNotFound = () => import('@/components/PageNotFound');
 const routes = [
   { path: '/', name: 'Home', component: PageHome },
   {
-    path: '/thread/show/:id',
+    path: '/thread/:id',
     name: 'ThreadShow',
     component: PageThreadShow,
     props: true,
+    beforeEnter(to, from, next) {
+      const threadExists = sourceData.threads.find(
+        (thread) => thread.id === to.params.id
+      );
+
+      if (threadExists) {
+        return next();
+      } else {
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          query: to.query,
+          hash: to.hash,
+        });
+      }
+    },
   },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: PageNotFound },
 ];
